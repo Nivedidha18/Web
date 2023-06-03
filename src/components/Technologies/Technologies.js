293d8fb8109a1f3f2aee1d52e725bc5576a4c94d
Nivedidha Kumaravelu <105373116+Nivedidha18@users.noreply.github.com>
2023-06-03 11:45:53 +0100

    Update Technologies.js

--- src/components/Technologies/Technologies.js
+++ src/components/Technologies/Technologies.js
@@ -23,15 +23,7 @@ const Technologies = () => (
     <br />
     <SectionTitle>Technologies</SectionTitle>
     <SectionText>
-      As a Master's student in Software Engineering with 2+ years of experience
-      in various programming languages and technologies, my objective is to
-      secure a Software Engineer/Java developer/Python Developer role where I
-      can utilize my skills and experience to contribute to the development of
-      innovative software solutions. I am eager to work in a dynamic and
-      challenging environment that encourages continuous learning and
-      professional growth. With my ability to write quality code under pressure
-      and collaborate effectively with team members, I am confident in my
-      ability to add value to any organization.
+     I have expertise in a wide range of programming languages, including Java, Python, Spring Boot, HTML/CSS, JavaScript, SQL, R ggplot2, and PHP. These languages enable me to develop robust and efficient software solutions across various domains. Additionally, I am experienced in using Linux and Shell scripting Bash/Zsh for automating tasks and managing systems. I am proficient in Tableau for data analysis and visualization, and I utilize Git, Bitbucket, and Trello for version control and collaboration. In terms of soft skills, I excel in time management, teamwork, problem-solving, and documentation. I have a logical and analytical mindset, allowing me to approach challenges with innovative solutions. With my natural leadership abilities and agile mindset, I can adapt to changing project requirements and deliver results efficiently. Effective communication, both written and verbal, is one of my strengths, enabling clear information exchange. I am skilled in multitasking and can handle multiple projects simultaneously. I am comfortable working under pressure and can maintain focus to deliver quality results. Building positive and collaborative relationships within a team is important to me, and I value interpersonal skills. Finally, I am confident in my ability to deliver engaging presentations that effectively communicate complex technical concepts to diverse audiences. Overall, I am a well-rounded technology professional with strong technical skills and personal attributes that make me capable of contributing effectively to software development projects.
     </SectionText>
     <List>
       <ListItem>
